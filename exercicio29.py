1cb5e4e0ec561e6ece277f609bbeed1a14925a78
baconzitus <thiagolds50@gmail.com>
2020-08-31 14:42:37 -0300

    feat: adicionando exercício 29

---
+++ exercicio29.py
@@ -0,0 +1,7 @@
+numero1  =int(input("valor 1: "))
+numero2  =int(input("valor 2: "))
+numero3  =int(input("valor 3: "))
+numero4  =int(input("valor 4: "))
+
+media = (numero1+numero2+numero3+numero4)/4
+print("media:",  media)
